ed818866cb43a8e52c8feeefb266772e81bcbdfa
Fabio <fabio.barth@web.de>
2023-02-08 10:31:28 +0100

    offer detail

--- src/app/pages/angebot/angebot-bearbeiten/angebot-bearbeiten.component.ts
+++ src/app/pages/angebot/angebot-bearbeiten/angebot-bearbeiten.component.ts
@@ -3,76 +3,97 @@ import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 import {environment} from "../../../../environments/environment";
 import {FormControl, FormGroup} from "@angular/forms";
+import {formatDate} from "@angular/common";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
-    selector: 'app-angebot-bearbeiten',
-    templateUrl: './angebot-bearbeiten.component.html',
-    styleUrls: ['./angebot-bearbeiten.component.scss']
+  selector: 'app-angebot-bearbeiten',
+  templateUrl: './angebot-bearbeiten.component.html',
+  styleUrls: ['./angebot-bearbeiten.component.scss']
 })
 export class AngebotBearbeitenComponent implements OnInit {
 
-    breadcrumbItems = [
-        {label: "Home", route: '/'},
-        {label: "Übersicht", route: './..'},
-        {label: "Detail", route: './..'},
-    ]
+  breadcrumbItems = [
+    {label: "Home", route: '/'},
+    {label: "Übersicht", route: './..'},
+    {label: "Detail", route: './..'},
+  ]
+  offerAccepted!: boolean
+  offerCompleted!: boolean
+  offer = new FormGroup({
+    name: new FormControl({value: '', disabled: true}),
+    email: new FormControl({value: '', disabled: true}),
+    lastName: new FormControl({value: '', disabled: true}),
+    description: new FormControl({value: '', disabled: true}),
+    street: new FormControl({value: '', disabled: true}),
+    postal: new FormControl({value: '', disabled: true}),
+    city: new FormControl({value: '', disabled: true}),
+    deadline: new FormControl({value: '', disabled: true}),
+    levy: new FormControl({value: '', disabled: true}),
+    price: new FormControl({value: '', disabled: true}),
+    partCount: new FormControl({value: '', disabled: true}),
+    workingHours: new FormControl({value: '', disabled: true}),
+    startDate: new FormControl({value: '', disabled: true}),
+    completionDeadline: new FormControl({value: '', disabled: true}),
+    machine: new FormControl({value: '', disabled: true}),
+    descriptionInquiry: new FormControl({value: '', disabled: true}),
+    partsDeliveryDate: new FormControl({value: '', disabled: true}),
+    deadlineDate: new FormControl({value: '', disabled: true}),
+  })
+  title!: string
+  id!: string
 
-    offer = new FormGroup({
-        name: new FormControl({value: '', disabled: true}),
-        email: new FormControl({value: '', disabled: true}),
-        lastName: new FormControl({value: '', disabled: true}),
-        description: new FormControl({value: '', disabled: true}),
-        street: new FormControl({value: '', disabled: true}),
-        postal: new FormControl({value: '', disabled: true}),
-        city: new FormControl({value: '', disabled: true}),
-        deadline: new FormControl({value: '', disabled: true}),
-        levy: new FormControl({value: '', disabled: true}),
-        price: new FormControl({value: '', disabled: true}),
-        partCount: new FormControl({value: '', disabled: true}),
-        workingHours: new FormControl({value: '', disabled: true}),
-        startDate: new FormControl({value: '', disabled: true}),
-        completionDeadline: new FormControl({value: '', disabled: true}),
-        machine: new FormControl({value: '', disabled: true}),
+  constructor(private client: HttpClient, private activeRoute: ActivatedRoute, private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    this.activeRoute.data.subscribe(value => {
+      this.title = value['title']
     })
-    title!: string
-    id!: string
 
-    constructor(private client: HttpClient, private activeRoute: ActivatedRoute) {
-    }
+    this.activeRoute.params.subscribe(value => {
+      this.id = value['id']
+    })
 
-    ngOnInit(): void {
-        this.activeRoute.data.subscribe(value => {
-            this.title = value['title']
-        })
-
-        this.activeRoute.params.subscribe(value => {
-            this.id = value['id']
-        })
-
-        this.client.get(environment.backend + '/offer/' + this.id + '/detail', {withCredentials: true}).subscribe(value => {
-            console.log(value)
-            this.offer.controls['name'].setValue(value['contact']['firstName'])
-            this.offer.controls['lastName'].setValue(value['contact']['lastName'])
-            this.offer.controls['email'].setValue(value['contact']['email'])
-            this.offer.controls['description'].setValue(value['description'])
-            this.offer.controls['street'].setValue(value['contact']['street'])
-            this.offer.controls['postal'].setValue(value['contact']['postalCode'])
-            this.offer.controls['city'].setValue(value['contact']['city'])
-            this.offer.controls['deadline'].setValue(value['inquiry']['deadlineDate'])
-            this.offer.controls['levy'].setValue(value['inquiry']['partsDeliveryDate'])
-            this.offer.controls['price'].setValue(value['price'])
-            this.offer.controls['partCount'].setValue(value['partCount'])
-            this.offer.controls['workingHours'].setValue(value['workingHours'])
-            this.offer.controls['startDate'].setValue(value['startDate'])
-            this.offer.controls['completionDeadline'].setValue(value['completionDeadline'])
-            this.offer.controls['machine'].setValue(value['machine'])
-        })
+    this.client.get(environment.backend + '/offer/' + this.id + '/detail', {withCredentials: true}).subscribe(value => {
+      this.offerAccepted = value['accepted']
+      this.offerCompleted = value['completed']
+      console.log(value)
+      this.offer.controls['name'].setValue(value['contact']['firstName'])
+      this.offer.controls['lastName'].setValue(value['contact']['lastName'])
+      this.offer.controls['email'].setValue(value['contact']['email'])
+      this.offer.controls['description'].setValue(value['description'])
+      this.offer.controls['street'].setValue(value['contact']['street'])
+      this.offer.controls['postal'].setValue(value['contact']['postalCode'])
+      this.offer.controls['city'].setValue(value['contact']['city'])
+      this.offer.controls['deadline'].setValue(value['inquiry']['deadlineDate'])
+      this.offer.controls['levy'].setValue(value['inquiry']['partsDeliveryDate'])
+      this.offer.controls['price'].setValue(value['price'])
+      this.offer.controls['partCount'].setValue(value['partCount'])
+      this.offer.controls['workingHours'].setValue(value['workingHours'])
+      this.offer.controls['startDate'].setValue(formatDate(value['startDate'], 'dd.MM.yyyy', 'en_US'))
+      this.offer.controls['completionDeadline'].setValue(formatDate(value['completionDeadline'], 'dd.MM.yyyy', 'en_US'))
+      this.offer.controls['machine'].setValue(value['machine'])
+      this.offer.controls['descriptionInquiry'].setValue(value['inquiry']['description'])
+      this.offer.controls['partsDeliveryDate'].setValue(formatDate(value['inquiry']['partsDeliveryDate'], 'dd.MM.yyyy', 'en_US'))
+      this.offer.controls['deadlineDate'].setValue(formatDate(value['inquiry']['deadlineDate'], 'dd.MM.yyyy', 'en_US'))
+    })
 
 
-    }
+
+  }
 
 
-    delete() {
+  delete() {
 
-    }
+  }
+
+  finishOffer() {
+    this.client.put(environment.backend + '/offer/' + this.id + '/completed', {}, {withCredentials: true}).subscribe(value => {
+      this.toastr.success('Angebot wurde abgeschlossen')
+      this.offerCompleted = true
+    }, error => {
+      this.toastr.error(error.error.message, 'Fehler')
+    })
+  }
 }
--- src/app/pages/seminare/seminare-bearbeiten/seminare-bearbeiten.component.ts
+++ src/app/pages/seminare/seminare-bearbeiten/seminare-bearbeiten.component.ts
@@ -78,13 +78,6 @@ export class SeminareBearbeitenComponent implements OnInit {
     })
   }
 
-  reformatDate(date) {
-    const parts = date.split(".");
-    const year = parseInt(parts[2], 10);
-    const month = parseInt(parts[1], 10) - 1;
-    const day = parseInt(parts[0], 10);
-    return new Date(year, month, day).toDateString();
-  }
 
 
   post() {
